a89df2f95fca93ea0aba0e5714f87b95a771c43d
micmine <micmine4@gmail.com>
2020-06-22 17:19:42 +0200

    :construction: Start with galery

---
+++ src/views/Galery.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="galery">
+		<li v-for="image in images" :key="image.uuid">
+			<img :src="image.source">
+		</li>
+	</div>
+</template>
+
+<script>
+	import axios from 'axios';
+
+	let images;
+
+	axios.get("http://localhost:8000/image/list").then((resp) => {
+		images = resp.data;
+		console.log(images);
+	}).catch((error) => {
+		console.error(error);
+	});
+
+	export default {
+		data() {
+			return {
+				"images": images
+			}
+		}
+	}
+
+</script>
