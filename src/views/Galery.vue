c955a8bbe2979827dedd4cc3b9c03fda8d999402
micmine <micmine4@gmail.com>
2020-06-23 19:35:10 +0200

    :sparkles: Add image title

--- src/views/Galery.vue
+++ src/views/Galery.vue
@@ -1,14 +1,18 @@
 <template>
 	<div id="galery">
 		<form method="POST" enctype='multipart/form-data' action="http://localhost:8000/image/upload">
+			<input type="text" name="title" required>
 			<input type="file" name="image" required>
 			<input type="hidden" name="redirect" value="http://localhost:8080">
 			<input type="submit" value="Upload">
 		</form>
 
 		<li v-for="image in images" :key="image.uuid">
-			<img @contextmenu.prevent="$refs.menu.open($event, { uuid: image.uuid })" :src="image.source"
-				:alt="image.title">
+			<div>
+				<p>{{ image.title }}</p>
+				<img @contextmenu.prevent="$refs.menu.open($event, { uuid: image.uuid })" :src="image.source" :alt="image.title">
+			</div>
+
 		</li>
 
 		<vue-context ref="menu">
@@ -29,54 +33,53 @@
 </template>
 
 <style lang="scss" scoped>
-	li {
-		list-style: none;
+li {
+	list-style: none;
 
-		img {
-			width: 500px;
-			z-index: 4;
-		}
-
+	img {
+		width: 500px;
+		z-index: 4;
 	}
+
+}
 </style>
 
 <script>
-	import axios from 'axios';
-	import VueContext from 'vue-context';
-	import 'vue-context/src/sass/vue-context.scss';
+import axios from 'axios';
+import VueContext from 'vue-context';
+import 'vue-context/src/sass/vue-context.scss';
 
-	export default {
-		data() {
-			return {
-				"images": []
-			}
-		},
-		components: {
-			VueContext
-		},
-		methods: {
-			onClick(text, uuid) {
-				console.log(text, uuid);
-				if (text === "edit") {
-					//sd
-				} else if (text === "remove") {
-					axios.put("http://localhost:8000/image", {
-						"uuid": uuid
-					}).then((resp) => {
-						console.log(resp.data);
-					}).catch((error) => {
-						console.error(error);
-					});
-				}
-			}
-		},
-		beforeCreate() {
-			axios.get("http://localhost:8000/image/list").then((resp) => {
-				this.images = resp.data;
-				console.log(resp.data);
-			}).catch((error) => {
-				console.error(error);
-			});
+export default {
+	data() {
+		return {
+			"images": []
 		}
+	},
+	components: {
+		VueContext
+	},
+	methods: {
+		onClick(text, uuid) {
+			console.log(text, uuid);
+			if (text === "edit") {
+				//sd
+			} else if (text === "remove") {
+				axios.put("http://localhost:8000/image", uuid
+				).then((resp) => {
+					console.log(resp.data);
+				}).catch((error) => {
+					console.error(error);
+				});
+			}
+		}
+	},
+	beforeCreate() {
+		axios.get("http://localhost:8000/image/list").then((resp) => {
+			this.images = resp.data;
+			console.log(resp.data);
+		}).catch((error) => {
+			console.error(error);
+		});
 	}
+}
 </script>
