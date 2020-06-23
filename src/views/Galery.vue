e30eb494d053a9e5ababd5b60b92961e110d1364
micmine <micmine4@gmail.com>
2020-06-23 19:01:18 +0200

    :construction: Add context menu

--- src/views/Galery.vue
+++ src/views/Galery.vue
@@ -7,52 +7,76 @@
 		</form>
 
 		<li v-for="image in images" :key="image.uuid">
-			<img :src="image.source" :alt="image.title">
+			<img @contextmenu.prevent="$refs.menu.open($event, { uuid: image.uuid })" :src="image.source"
+				:alt="image.title">
 		</li>
+
+		<vue-context ref="menu">
+			<template slot-scope="child">
+				<li>
+					<a @click.prevent="onClick($event.target.innerText, child.data)">
+						edit
+					</a>
+				</li>
+				<li>
+					<a @click.prevent="onClick($event.target.innerText, child.data)">
+						remove
+					</a>
+				</li>
+			</template>
+		</vue-context>
 	</div>
 </template>
 
 <style lang="scss" scoped>
-li {
-	list-style: none;
+	li {
+		list-style: none;
 
-	img {
-		width: 500px;
-		z-index: 4;
+		img {
+			width: 500px;
+			z-index: 4;
+		}
 
-		-webkit-user-drag: none;
-		-khtml-user-drag: none;
-		-moz-user-drag: none;
-		-o-user-drag: none;
-		user-drag: none;
 	}
-
-	img:active {
-		display: relative;
-		top: 50px;
-		width: 80%;
-		z-index: 5;
-	}
-}
 </style>
 
 <script>
-import axios from 'axios';
+	import axios from 'axios';
+	import VueContext from 'vue-context';
+	import 'vue-context/src/sass/vue-context.scss';
 
-export default {
-	data() {
-		return {
-			"images": []
+	export default {
+		data() {
+			return {
+				"images": []
+			}
+		},
+		components: {
+			VueContext
+		},
+		methods: {
+			onClick(text, uuid) {
+				console.log(text, uuid);
+				if (text === "edit") {
+					//sd
+				} else if (text === "remove") {
+					axios.put("http://localhost:8000/image", {
+						"uuid": uuid
+					}).then((resp) => {
+						console.log(resp.data);
+					}).catch((error) => {
+						console.error(error);
+					});
+				}
+			}
+		},
+		beforeCreate() {
+			axios.get("http://localhost:8000/image/list").then((resp) => {
+				this.images = resp.data;
+				console.log(resp.data);
+			}).catch((error) => {
+				console.error(error);
+			});
 		}
-	},
-	beforeCreate() {
-		axios.get("http://localhost:8000/image/list").then((resp) => {
-			this.images = resp.data;
-			console.log(resp.data);
-		}).catch((error) => {
-			console.error(error);
-		});
 	}
-}
-
 </script>
