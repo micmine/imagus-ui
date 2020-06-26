5cad112dc0ca1810ffb242f7d5a0272d080cc521
micmine <micmine4@gmail.com>
2020-06-26 18:32:38 +0200

    :lipstick: Style edit template

--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -1,6 +1,17 @@
 <template>
 	<div class="edit" v-bind:class="{notActive: !isActive }">
-		<button v-on:click="submit">Submit</button>
+		<div class="from">
+			<input class="input" type="text" v-model="image.title" placeholder="title">
+
+			<div class="field is-grouped action">
+				<div class="control">
+					<button class="button is-link" v-on:click="submit">Submit</button>
+				</div>
+				<div class="control">
+					<button class="button is-link is-light" v-on:click="hide">Cancel</button>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -14,9 +25,20 @@
 	top: 20vh;
 	left: 5vw;
 	right: 5vw;
-	bottom: 20vh;
 	z-index: 100;
-	background-color: red;
+	background-color: #282C34;
+	border-radius: 5px;
+
+	.from {
+		margin-top: 1vh;
+		margin-left: 1vw;
+		margin-right: 1vw;
+		margin-bottom: 1vh;
+
+		.action {
+			padding-top: 1vh;
+		}
+	}
 }
 </style>
 
@@ -27,30 +49,31 @@ export default {
 	props: ["uuid", "isActive"],
 	data() {
 		return {
-			data: {
-				image: {}
-			}
+			image: {}
 		}
 	},
-	beforeUpdate: function () {
-		console.log({ data:  { "uuid": this.uuid } });
-		axios({
-			method: "get",
-			url: "http://localhost:8000/image",
-			params: { 
-				uuid: this.uuid 
-			}
-		}).then((resp) => {
+	methods: {
+		submit: function () {
+			this.hide();
+		},
+		hide: function () {
+			this.isActive = false;
+		},
+		load: function () {
+			axios.post("http://localhost:8000/image", { "uuid": this.uuid }).then((resp) => {
 				this.image = resp.data;
 				console.log(resp.data);
 			}).catch((error) => {
 				console.error(error);
 			});
+		}
 	},
-	methods: {
-		submit: function () {
-			this.isActive = false
-		},
-	},
+	watch: {
+		isActive: function () {
+			if (this.isActive == true) {
+				this.load();
+			}
+		}
+	}
 }
 </script>>
