e08696b57876748f67471cb45bdad60936c0ee41
micmine <micmine4@gmail.com>
2020-06-27 15:44:05 +0200

    :sparkles: Add keycloak

--- src/main.js
+++ src/main.js
@@ -2,13 +2,40 @@ import Vue from "vue";
 import store from  "./store";
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
+import VueKeyCloak from "@dsb-norge/vue-keycloak-js";
 
 Vue.config.productionTip = false;
 
-new Vue({
-	router,
-	store,
-	render: (h) => h(App),
-}).$mount("#app");
+function tokenInterceptor () {
+	axios.interceptors.request.use(config => {
+		config.headers.Authorization = `Bearer ${Vue.prototype.$keycloak.token}`
+		return config
+	}, error => {
+		return Promise.reject(error)
+	})
+}
+
+
+Vue.use(VueKeyCloak, {
+	init: {
+		onLoad: "login-required",
+		checkLoginIframe: false
+	},
+	config: {
+		realm: "master",
+		url: "http://localhost:8080/auth",
+		clientId: "imagus-ui"
+	},
+	onReady: () => {
+		tokenInterceptor();
+
+		new Vue({
+			router,
+			store,
+			render: h => h(App)
+		}).$mount("#app")
+	}
+})
 
 
