96bd487687995c1eab00359fac1bb637560cfda9
micmine <micmine4@gmail.com>
2020-06-27 19:35:36 +0200

    :lipstick: Add Loading Spinner

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
 	<div id="app">
+		<rotate-square5 class="spinner" v-if="loading"></rotate-square5>
 		<button class="button logout" @click="$keycloak.logoutFn" v-if="$keycloak.authenticated">Log out</button>
 		<router-view/>
 	</div>
 </template>
 
-<style>
+<style lang="scss">
 @import "~bulma/css/bulma.css";
 #app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -21,4 +22,32 @@ body {
 .logout {
 	float: right;
 }
+
+.spinner {
+	position: fixed;
+	top: 20vh;
+	left: 49%;
+	right: 5vw;
+	z-index: 100;
+	background-color: white;
+	padding: 20px;
+	border-radius: 10px;
+}
 </style>
+
+<script>
+import {RotateSquare5} from "vue-loading-spinner";
+import store from "./store";
+
+export default {
+	computed: {
+		loading: function () {
+			return store.state.loadingStatus;
+		}
+	},
+	components: {
+		RotateSquare5
+	}
+	
+}
+</script>>
